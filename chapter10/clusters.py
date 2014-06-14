3e697394b3bcbee76cd959daf9b076d1cf6fe6fd
Clemens Helm <clemens.helm@gmail.com>
2014-06-14 08:12:28 +0200

    Added tanimoto distance from book.

--- chapter10/clusters.py
+++ chapter10/clusters.py
@@ -22,6 +22,16 @@ def pearson(v1,v2):
   if den==0: return 0
 
   return 1.0-(num/den)
+  
+def tanimoto(v1,v2):
+  c1,c2,shr=0,0,0
+
+  for i in range(len(v1))
+    if v1[i]!=0: c1+=1 # in v1
+    if v2[i]!=0: c2+=1 # in v2
+    if v1[i]!=0 and v2[i]!=0: shr+=1 # in both
+
+  return 1.0-(float(shr)/c1+c2-shr)
 
 
 class bicluster:
